7d49102bfe68ce9cadae18401ddb1d95526f935d
Richard Backhouse <richardabackhouse@gmail.com>
2013-08-27 05:48:01 -0400

    Add License

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,8 @@
+/*
+    Copyright (c) 2004-2013, The Dojo Foundation All Rights Reserved.
+    Available via Academic Free License >= 2.1 OR the modified BSD license.
+    see: http://dojotoolkit.org/license for details
+*/
 define(['jquery', 'mobileconfig', 'routers/router'], function($, mobileconfig, router) {
     $(document).ready(function() {
 		console.log("ready");
--- resources/js/mobileconfig.js
+++ resources/js/mobileconfig.js
@@ -1,3 +1,8 @@
+/*
+    Copyright (c) 2004-2013, The Dojo Foundation All Rights Reserved.
+    Available via Academic Free License >= 2.1 OR the modified BSD license.
+    see: http://dojotoolkit.org/license for details
+*/
 define(['jquery'], function($) {
 	$(document).bind("mobileinit", function() {
 	    $.mobile.linkBindingEnabled = false;
